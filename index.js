1ccccdbd5bfcf6a71a67cdb379fab6b8338ee28e
Melanie <melanie.collins.p@gmail.com>
2024-05-23 18:54:02 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import 'dotenv/config'
+import express from 'express'
+
+export const app = express()
+
+const __dirname = import.meta.dirname
+app.use(express.static(__dirname + '/public'))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+app.use('*', (_, res) => {
+    res.status(404).json({ ok: false, msg: 'ruta no configurada 😐' })
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Servidor encendido http://localhost:${PORT}`)
+})
