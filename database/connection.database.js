1b6e3c48936a662269d84b495c952f5ce6c969ac
Melanie <melanie.collins.p@gmail.com>
2024-05-25 17:28:15 -0400

    conexión a database

---
+++ database/connection.database.js
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import pg from "pg";
+const { Pool } = pg;
+
+const connectionString = process.env.CONNECTION_STRING;
+
+export const pool = new Pool({
+    connectionString,
+    allowExitOnIdle: true,
+});
+
+try {
+    await pool.query("SELECT NOW()");
+    console.log("conexión a postgres");
+} catch (e) {
+    console.log(e);
+}
+
+// Probar en la terminal: node database/connection.database.js
--- index.js
+++ index.js
@@ -9,6 +9,9 @@ app.use(express.static(__dirname + '/public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+})
 
 app.use('*', (_, res) => {
     res.status(404).json({ ok: false, msg: 'ruta no configurada 😐' })
